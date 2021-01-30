2d54366b7e485dc03f24316c4ad96c5380eb3ea5
alienx456 <estebanelias27dev@gmail.com>
2021-01-30 18:14:36 -0500

    Added configuration files

---
+++ terraform/lambda_permission.tf
@@ -0,0 +1,22 @@
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.func.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.bucket.arn
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.func.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
