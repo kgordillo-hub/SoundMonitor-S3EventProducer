6d9191e972d4bc7b8c60ba82b3fe4d9a29534ef0
alienx456 <estebanelias27dev@gmail.com>
2021-01-30 20:14:28 -0500

    solving, function not being updated

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "func" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
   runtime       = "nodejs12.x"
-  source_code_hash = filebase64sha256(var.filename)
+  publish = true
 
   depends_on = [
     aws_iam_role_policy_attachment.lambda_logs,
