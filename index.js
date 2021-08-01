a9626d0dfeaae099e4003535757551c0195cc9d3
alienx456 <estebanelias27dev@gmail.com>
2021-08-01 12:15:45 -0500

    solve sonar

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ exports.handler = (event, context, callback) => {
 
     console.log("Reading options from event = \n", util.inspect(event, {depth: 7}));
 
-    for (item of map) {
+    for (let item of map) {
         if (item.bucketName === event.Records[0].s3.bucket.name) {
             const fileName = event.Records[0].s3.object.key;
             console.log("Uploaded data : ",fileName);
 
             var kafkaEndpoints = process.env.KAFKA_ENDPOINTS.split(',');
-        
+
             const kafka = new Kafka({
                 brokers: kafkaEndpoints
             });
-        
+
             const producer = kafka.producer()
-        
+
             const run = async () => {
                 await producer.connect()
                 await producer.send({
@@ -31,6 +31,6 @@ exports.handler = (event, context, callback) => {
             }
             run().catch(console.error);
             break;
-        } 
+        }
     }
 }
