a5d413e74f7565f2fc61789267943e0d083c1793
kgordillo-hub <ksgordilloo@correo.udistrital.edu.co>
2022-12-05 19:32:40 -0500

    Update reporter.tf

--- IAC/reporter.tf
+++ IAC/reporter.tf
@@ -1,5 +1,5 @@
 module "reporter" {
-  source = "git::https://github.com/kgordillo-hub/SoundMonitor-IAC-Infrastructure-Common.git//lambda_s3_kafka?ref=2.2.0"
+  source = "git::https://github.com/kgordillo-hub/SoundMonitor-IAC-Infrastructure-Common.git//lambda_s3_kafka?ref=4.0.0"
 
   aws_provider_key= var.aws_provider_key
   aws_provider_secret= var.aws_provider_secret
